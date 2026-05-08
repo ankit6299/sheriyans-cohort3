dbb827308e04b628532b7e60fb3eb09edee07044
ankit6299 <ankit6299659@gmail.com>
2026-05-08 16:47:31 +0530

    updated till day

--- dsa-js/app.js
+++ dsa-js/app.js
@@ -58,8 +58,8 @@
 
 //swapping vars
 
-let a = 120;
-let b = 202;
+// let a = 120;
+// let b = 202;
 // console.log(`${a} ${b}`);
 
 // a = a + b;
@@ -67,5 +67,33 @@ let b = 202;
 // a = a - b;
 // console.log(`${a} ${b}`);
 
-[a,b] = [b,a];
-console.log(`${a} ${b}`);
+// [a,b] = [b,a];
+// console.log(`${a} ${b}`);
+
+//Skipped Operator Video
+
+//Math library
+
+// console.log(Math.abs(-123123));
+// console.log(Math.ceil(10.2));
+// console.log(Math.floor(10.9));
+// console.log(Math.round(0.5));
+// console.log(Math.round(0.4));
+// console.log(1231.213.toFixed(1));
+// console.log(Math.random());
+
+let prompt = require("prompt-sync")();
+// let p = prompt("Enter Principle:");
+// let r = prompt("Enter Rate:");
+// let t = prompt("Enter time:");
+
+// console.log((p*Math.pow(1+(r/100),t)-p).toFixed(2));
+
+//OTP
+//1000-9999
+console.log(Math.trunc((Math.random()*9000)+1000)); //0+1000 to 8999
+
+
+
+
+
---
+++ html_live/markdown/e.md
@@ -0,0 +1,4 @@
+SEO - HTML
+AI - Text
+
+Markdown
